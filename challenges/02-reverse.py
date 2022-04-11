45c9bc37a67acb4b69efeebd006349600625c8b5
katherine-kania <katherine.kania@gmail.com>
2022-04-11 17:43:22 -0400

    02-Reverse added

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,11 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+def reverse(string):
+    rev_string = ""
+    for letter in string:
+        rev_string = letter + rev_string
+
+    return rev_string
+print(reverse("katherine"))
