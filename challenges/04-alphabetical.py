5eb8f2b51c3a955c8d0ec9b18e97b1a1b049108a
patrickga <patrick.g.alvarez@gmail.com>
2022-04-11 18:50:45 -0500

    Challenge 4 completed

--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,19 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+from hashlib import new
+from ntpath import join
+from posixpath import split
+
+
+def alphabetize(string):
+    new_list = []
+    for char in string:
+        new_list.append(char)
+    ordered_list = sorted(new_list)
+    print(''.join(ordered_list))
+
+user_string = input("Give me a string to alphabetize\n")
+
+alphabetize(user_string)
