74f9b7bd88702c62d859f5bc82436d3ad7185b6e
chrislee1996 <prepchrislee@gmail.com>
2022-04-11 18:57:16 -0400

    challenge 4 completed

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,18 @@
 print("Welcome to Chase bank.")
 print("Have a nice day!")
 
+balance = 4000
+choice = input("What would you like to do? (deposit, withdraw, check_balance: ")
+
+if (choice == "deposit"):
+    deposit = int(input("How much would you like to deposit?: "))
+    balance = balance + deposit
+    print(f"Your current balance is: ${balance}")
+elif (choice == "withdraw"):
+    withdraw = int(input("How much would you like to withdraw?: "))
+    balance = balance - withdraw
+    print(f"Your current balance is: ${balance}")
+elif (choice == "check_balance"):
+    print(f"Your current balance is: ${balance}")
+else:
+    print("Please enter a valid method")
