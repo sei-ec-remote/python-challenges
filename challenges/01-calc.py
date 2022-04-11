089949d01d8f2cb1fa3416c6070fb166d9d4b480
patrickga <patrick.g.alvarez@gmail.com>
2022-04-11 17:50:03 -0500

    Challenge 1 complete

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,21 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+method = input("What calculation would you like to do? (add, sub, mult, div)")
+print(method)
+num1 = input("What is number 1?")
+print(num1)
+num2 = input("What is number 2?")
+print(num2)
+
+if method == "add":
+    print(f"Your result is {int(num1) + int(num2)}")
+elif method == "sub":
+    print(f"Your result is {int(num1) - int(num2)}")
+elif method == "mult":
+    print(f"Your result is {int(num1) * int(num2)}")
+elif method == "div":
+    print(f"Your result is {int(num1) / int(num2)}")
+
+
