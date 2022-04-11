1391bee3204e872c1b972d050c80fc6eeec67199
patrickga <patrick.g.alvarez@gmail.com>
2022-04-11 18:00:40 -0500

    Challenge 2 complete

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,21 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+
+from posixpath import split
+
+
+string = input("Enter a string: ")
+
+def reverse_string(sample_string):
+    string_list = split(sample_string)
+    reversed = []
+    i = len(sample_string) - 1
+    while i > -1:
+        reversed.append(sample_string[i])
+        i -= 1
+    reversed_string = ''.join(reversed)
+    print(reversed_string)
+
+reverse_string(string)
