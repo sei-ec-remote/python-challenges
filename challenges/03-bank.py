79922a09aed87218e993acb5362b7e67d920b788
johnzwiss <johnzwiss@gmail.com>
2022-04-11 16:59:16 -0400

    finished challenge 3

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,15 @@
 print("Welcome to Chase bank.")
-print("Have a nice day!")
+balance = 4000
+method = input("what would you like to do? (deposit, withdraw, check_balance)")
+if (method == "check_balance"):
+    print(balance)
+elif (method == "withdraw"):
+    withdrawamt = int(input("how much?"))
+    print(f"your current balance is: {balance - withdrawamt}")
+elif (method == "deposit"):
+    depositamt = int(input("how much?"))
+    print(f"your current balance is: {balance + depositamt}")
+else: 
+    print("invalid option")
+
 
