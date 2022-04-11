568bbbb7c70f86a77837b3c2a2f5d3a386a31b8a
ldomino <domino.laurie@gmail.com>
2022-04-11 18:08:23 -0500

    Reverse a string using python and using a new string and for loop

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,21 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+
+# Reverse a string using a For loop and a new string.
+#
+
+choice = input("Input a string: \n")
+
+# Setup holding the output reverse string
+reverse_string = ''
+
+# Start at length of choice, go to 0 index, -1 
+#   reverse_string is the previous reverse_string plus
+#     the choice[i-1] as i starts at length and index is always
+#     1 less.   So reverse_string starts with last letter
+#   reverse_string = reverse_string + choice[i -1]
+for i in range(len(choice), 0, -1):
+   reverse_string += choice[i-1]
+print (reverse_string)
