4494b467f8ee2466a7a3a367310c9c268f9b6f79
thefrostyausty <austinrobinson0000@gmail.com>
2022-04-11 20:41:33 -0400

    challenge 3: completed

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,21 @@
-print("Welcome to Chase bank.")
-print("Have a nice day!")
+
+def bank_transaction(starting_balance):
+    print("Welcome to Chase bank.")
+    print(f"Your starting balance is: {starting_balance}") 
+    balance = starting_balance
+    prompt = input("What would you like to do? (Deposit, Withdraw, Check Balance):")
+    if prompt == "Deposit" or prompt == "deposit":
+        amount = input("How much would you like to Deposit:")
+        balance += int(amount)
+        print(f"Your current balance is: {balance}")
+    elif prompt == "Withdraw" or prompt == "withdraw":
+        amount = input("How much would you like to withdraw:")
+        balance -= int(amount)
+        print(f"Your current balance now is: {balance}")
+    elif prompt == "Check Balance" or prompt == "check balance":
+        input(f"Here's your current balance:{balance}")
+        print("thank you for visiting")
+    else:
+        print("Thanks for visiting")
+bank_transaction(4000)
 
