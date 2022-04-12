76133bf75c1c5cd59927490c0d637d0c4d137a0e
dnierman0920 <davidnierman93@gmail.com>
2022-04-11 17:59:17 -0700

    finish 02-reverse

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,18 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+# Challenge 2 - Reverse a string
+# Reverse a string manually. Don't use s[::-1] (even though that's awesome).
+# Create a new variable storing an empty string and add the letters from the
+# first string one by one. The for loop should iterate over the length of the
+# string and you should access letters individually.
+
+
+def reverse_string(some_string):
+    new_string = []
+    for char in some_string:
+        new_string.insert(0, char)
+    return ''.join(new_string)
+
+print(reverse_string("reverse_me")) #should return em_esrever
