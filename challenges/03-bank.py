7fdcd46e5128923d1f54dd3e164bd68ebcee73f5
mitchgemma <mitchgemma@gmail.com>
2022-04-12 10:56:12 -0400

    finished challenge 3

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,15 @@
 print("Welcome to Chase bank.")
-print("Have a nice day!")
 
+def bank():
+    balance = input("Your current balance is:")
+    action = input("What would you like to do? (deposit, withdraw, check_balance):")
+    if action == 'check_balance':
+        print(balance)
+    elif action == 'deposit':
+        deposit = input('How much would you like to deposit?')
+        print(f"Your current balance is: {int(balance) + int(deposit)} ")
+    elif action == 'withdraw':
+        withdraw = input('How much would you like to withdraw?')
+        print(f"Your current balance is: {int(balance) - int(withdraw)} ")    
+    print("Have a nice day!")
+bank()
