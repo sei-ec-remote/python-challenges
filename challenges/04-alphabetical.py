87b29faae6224db6caa16c48f56c1fdfedda600c
jfw2855 <jwaddle1130@gmail.com>
2022-04-11 20:15:25 -0500

    alpha completed

--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,14 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+
+def alphabetize_str (str):
+    str_list = []
+    for x in range(len(str)):
+        str_list.append(str[x])
+    str_list.sort()
+    return "".join(str_list)
+
+
+print (alphabetize_str("supercalifragilisticexpialidocious"))
