971367407dbce2c205c4de70d059ca2eab9b35f2
NikPurvis <nikpurvis@gmail.com>
2022-04-11 21:44:49 -0600

    Completed problem 3

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,22 @@
+print("")
 print("Welcome to Chase bank.")
-print("Have a nice day!")
+print("")
 
+cur_bal = 4000
+
+action = input("What would you like to do?\n(check_balance, withdraw, deposit, quit)\n")
+
+if (action == "check_balance"):
+    print(f"Your current balance is: ${cur_bal}")
+elif (action == "withdraw"):
+    withdraw_amt = int(input("How much would you like to withdraw?\n"))
+    cur_bal -= withdraw_amt
+    print(f"Your current balance is: ${cur_bal}")
+elif (action == "deposit"):
+    deposit_amt = int(input("How much would you like to deposit?\n"))
+    cur_bal += deposit_amt
+    print(f"Your current balance is: ${cur_bal}")
+elif (action == "quit"):
+    print("Have a nice day!")
+else:
+    print("Please select a valid option.")
