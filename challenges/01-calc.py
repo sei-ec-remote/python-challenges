3d349e51bac6151009a5aa446760c3f578832e01
STEVIEMILITELLO <steviemilitello@gmail.com>
2022-04-11 21:26:51 -0400

    all challenges completed

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,18 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+calc = input ("what calculation would you like to do (add, sub, mult or div?: ")
+num1 = int(input ("what is number 1: "))
+num2 = int(input ("what is number 2: "))
+
+if calc == 'add':
+    result = num1 + num2
+elif calc == 'sub':
+    result =  num1 - num2
+elif calc == 'mult':
+    result = num1 * num2
+elif calc == 'div':
+    result = num1 / num2
+
+print(f"the result of {num1} and {num2} is {result}")
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,16 @@
 print("Welcome to Chase bank.")
 print("Have a nice day!")
 
+balance = 4000
+bank = input ("what would you like to do?: deposit, withdraw, check_balance, ")
+
+if bank == 'check_balance':
+    result = balance
+elif bank == 'deposit':
+    deposit = int(input ("how much would you like to deposit: "))
+    result = balance + deposit
+elif bank == 'withdraw':
+    withdraw = int(input ("how much would you like to withdraw: "))
+    result = balance - withdraw
+
+print(f"you have {result} in your account")
