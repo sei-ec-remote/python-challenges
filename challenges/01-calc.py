5fa6e72cb6238f2ca734977a4a4570377bca755a
jfw2855 <jwaddle1130@gmail.com>
2022-04-11 20:14:45 -0500

    calc completed

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,19 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+# Challenge 1 - Calculator
+# Create a simple calculator that first asks the user what method they would like to use 
+# (addition, subtraction, multiplication, division) and then asks the user for 
+# two numbers, returning the result of the method with the two numbers. Here is 
+# a sample prompt:
+
+
+method=input(" Hi, this is a simple calc! What method would you like to use: +,-,*,/ ")
+num1=input(" please enter num1 ")
+num2=input(" please enter num2 ")
+
+ans = eval(f"{num1}{method}{num2}")
+
+
+print(f'{num1} {method} {num2} = {ans}')
