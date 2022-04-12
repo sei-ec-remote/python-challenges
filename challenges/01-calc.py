3c649e7ef0be7cefba89740bb9bb471cf0dd8612
“BishrJones” <bishr6jones@gmail.com>
2022-04-11 18:53:25 -0700

    01 and 02 complete

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,30 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+def calc():
+    # declare all of the inputs I need
+    method = input('What calculation would you like to do? (add,sub.mult,div)')
+    # nums must use int to convert to number instead o string
+    num1 = int(input('What is the first number?'))
+    num2 = int(input('What is the second number?'))
+
+    #  declare the different types of math that can be refrenced later in the func
+    types = {
+        'add': num1 + num2,
+        'sub': num1 - num2,
+        'div': num1 / num2,
+        'mult': num1 * num2
+    }
+
+    # if statment that looks for method in  the user input that matches one of the types
+    if (method in types):
+        # if the method is found in the types then return the total of whatever method was chosen
+        return types[method]
+    else:
+        # catch for any error that occurs if method is not valid (or total is a decimal)
+        # prints the return as well as none(null)
+        return print('Input valid method')
+
+
+print(calc())
--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,16 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+# make a function with the parameter of string to refrence later
+def reverse(string):
+    # make a new string that is empty
+    new_string = ''
+    # for each item/letter in the string(parameter)
+    for item in string:
+        # the new_string is = to the string(parameter) added to the old new_string
+        new_string = item + new_string
+        # return the new new_string which should be backwards because the first   item in the string(parameter) will be the first input into the new_string adding a new item from right to left
+    return new_string
+
+print(reverse('me'))
