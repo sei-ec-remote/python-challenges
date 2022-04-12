0fc205376c1f44d36e786d655942d5b534dd2e64
Ivycle94 <Ivycle94@gmail.com>
2022-04-11 22:13:11 -0500

    alphabet working

--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,10 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+str = input('enter string')
+split_str = list(str)
+split_str.sort()
+sorted_list = ''.join(split_str)
+
+print(sorted_list) 
