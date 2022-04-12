2d844e2cb92e1bbbf9edd98930a68fc7f7dfa25d
Ivycle94 <Ivycle94@gmail.com>
2022-04-11 20:34:49 -0500

    calc working

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,18 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+op = input("Pick an operator (+, -, *, /)")
+n1 = int(input("enter first number"))
+n2 = int(input("enter second number"))
+
+if op == "+":
+    answer = n1 + n2
+elif op == "-":
+    answer = n1 - n2
+elif op == "x":
+    answer = n1 * n2
+elif op == "/":
+    answer = n1 / n2
+
+print(f"{n1} {op} {n1} = {answer}")
