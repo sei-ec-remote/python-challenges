3156d18ca83573346eceb24bfff6c7cc9eb62048
lyee197 <liamsyee@gmail.com>
2022-04-11 20:09:39 -0400

    Finished Challenge 4 - Sort a String

--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,12 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+string = input('Give me a string to alphabetize\n')
+
+def order(string):
+    array = list(string)
+    array.sort()
+    print("Alphabetized:",("".join(array)))
+
+order(string)
