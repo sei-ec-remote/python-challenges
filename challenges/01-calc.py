4e6545a6fc752f43b7c45d12d77f5f1553d3fae0
Nash4star <forrester.nash@gmail.com>
2022-04-18 18:43:29 -0400

    finished problem 1 and 2

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,18 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+calc = input('What type of problem would you like to do?')
+num1 = int(input('first number :'))
+num2 = int(input('second number :'))
+
+def calculator ():
+    if calc == 'add':
+        print (num1 + num2)
+    elif calc == 'sub':
+        print (num1 - num2)
+    elif calc == 'div':
+        print (num1 / num2)
+    elif calc == 'mult':
+        print (num1 * num2)
+calculator()
