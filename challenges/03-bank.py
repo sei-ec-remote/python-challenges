c372421834b0aeee9d5e11848fc56e2ee4ced4ae
Nash4star <forrester.nash@gmail.com>
2022-04-19 09:27:47 -0400

    problem 3 done

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,22 @@
 print("Welcome to Chase bank.")
-print("Have a nice day!")
 
+def bank(base_balance):
+    print(f'Your balance is: {base_balance}')
+    balance = base_balance
+    event = input('what would you like to do today?(Deposit, Withdraw, Check Balance) ')
+    if event == 'Deposit':
+        amount = input('What amount would you like to Deposit?')
+        balance += int(amount)
+        print(f'Your balance is now: {balance}')
+        return event
+    elif event == 'Withdraw':
+        amount = input('What amount would you like to Withdraw?')
+        balance -= int(amount)
+        print(f'Your balance is now: {balance}')
+        return event
+    elif event == 'Check Balance':
+        input(f'Your balance is: {balance}')
+    else:
+        print("Have a nice day!")
+
+bank(2000)
