5309102829cedc452c8eea14bde31ee66f0faee9
DRG104 <ph_kyle104@yahoo.com>
2022-08-10 17:00:11 -0500

    Banking complete

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,23 @@
 print("Welcome to Chase bank.")
+
+question = input("Please enter a selection: display balance, withdraw, deposit\n")
+
+balance = 500.00
+
+if question == 'display balance':
+    print(f'Your current balance is ${balance}')
+if question == 'withdraw':
+    withdraw = int(input("Please enter the amount you would like to withdraw:"))
+    if withdraw > 500.00:
+        print("Sorry, your balance is too low for this transaction.")
+    elif withdraw < 500.00:
+        new_balance = balance - withdraw
+        print("You're account balance is now:", f"${new_balance}")
+if question == 'deposit':
+    deposit = int(input("Please enter the amount you would like to deposit:"))
+    print(f"Transaction complete, your new balance is ${balance + deposit}")
+else: 
+    print("I'm sorry I don't understand, please try again.")
+
 print("Have a nice day!")
 
