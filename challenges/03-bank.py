eebebd9f4809052ee704a97c93bb46c21f069df3
Zene09 <zeneorr@gmail.com>
2022-08-10 15:25:20 -0500

    bank finished

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,20 @@
 print("Welcome to Chase bank.")
-print("Have a nice day!")
+
+ask = input('How can I help you today? Respond with: balance, withdraw, or deposit. ')
+
+balance = 1000
+
+# check balance
+if ask == 'balance':
+    print(f'Your account balance is: ${balance}')
+elif ask == 'withdraw':
+    subtract_balance = input('How much would you like to withdraw? You cannot exceed your account balance. $')
+    after_withdrawal = balance - int(subtract_balance)
+    print(f'Your new balance is: ${after_withdrawal}.')
+elif ask == 'deposit':
+    add_balance = input('How much would you like to deposit? $')
+    after_deposit = balance + int(add_balance)
+    print(f'Your new balance is ${after_deposit}.')
+else:
+    print("Have a nice day!")
 
