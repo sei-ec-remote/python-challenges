7a978d1428f8ecbff7576af8dce6d791b8c56f2d
Zene09 <zeneorr@gmail.com>
2022-08-10 14:27:24 -0500

    calculator finished

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,18 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+calculator = input('What calculation would you like to do? (add, subtract, multiply, divide) ')
+
+first_value = input('What is the first number? ')
+
+second_value = input('What is the second number? ')
+
+if calculator == 'add':
+    print(f'The sum is {int(first_value) + int(second_value)}')
+elif calculator == 'subtract':
+    print(f'The difference is {int(first_value) - int(second_value)}')
+elif calculator == 'multiply':
+    print(f'The product is {int(first_value) * int(second_value)}')
+else:
+    print(f'The quotient is {int(first_value) / int(second_value)}')
