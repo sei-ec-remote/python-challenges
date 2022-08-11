3e4376f69ba6da130a1e37a4b461bd4aa6328448
f-eiyu <feiyu@alum.mit.edu>
2022-08-11 22:33:08 +0200

    Finish reverse

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,10 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+str = input("Enter a string: ")
+str_rev = ""
+for i in range(len(str)):
+  str_rev += str[-i - 1]
+
+print(str_rev)
