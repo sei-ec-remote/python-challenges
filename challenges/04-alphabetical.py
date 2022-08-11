c7907126aa303ad6f5659c0e2a3b3122ae1dfffb
LarryMays92 <LarryMays92@ymail.com>
2022-08-10 20:11:59 -0400

    q4

--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,12 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+#Give me a string to alphabetize
+#supercalifragilisticexpialidocious
+#Alphabetized: aaacccdeefgiiiiiiillloopprrssstuux
+
+scramble = "supercalifragilisticexpialidocious"
+order = sorted(scramble)
+# print(order)
+together = "".join(order)
+print(together)
