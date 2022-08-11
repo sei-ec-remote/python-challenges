1ac461081019456479970250210c7f543c84ca4a
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2022-08-10 20:44:24 -0400

    finished

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -1,4 +1,31 @@
 # Use the `input()` function to prompt a user to enter something.
+
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+# Create a simple calculator that first asks the user what method they would like
+# to use (addition, subtraction, multiplication, division) and then asks the user
+# for two numbers, returning the result of the method with the two numbers. Here
+# is a sample prompt:
+
+# ```
+
+
+calculator = input('What calculation would you like to do?(add, sub, mult, div)')
+# add
+question_two = input('What is number 1?')
+# 3
+question_three = input('What is number 2?')
+# 6
+# Your result is 9
+# ```
+if calculator == 'add':
+    print("The sum of", question_two, "and", question_three, "=", int(question_two) + int(question_three))
+elif calculator == 'sub':
+    print("The remainder of", question_two, "and", question_three, "=", int(question_two) - int(question_three))
+elif calculator == 'mult':
+    print("The product of", question_two, "and", question_three, "=", int(question_two) * int(question_three))
+elif calculator == 'div':
+    print("The quotient of", question_two, "and", question_three, "=", int(question_two) / int(question_three))
+
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,72 @@
-print("Welcome to Chase bank.")
-print("Have a nice day!")
+# print("Welcome to Chase bank.")
+# print("Have a nice day!")
 
+# ### Challenge 3 - Bank Transactions
+# Create a prompt that asks the user if they would like to display their balance,
+# withdraw or deposit. Write three methods to perform these calculations and
+# output the result to the user.
+continue_ = 'yes'
+
+while continue_ == 'yes':
+
+    balance = 0
+
+    # def add_Balance(e):
+    #     num = e
+    #     print('add', num)
+
+    def sub_Balance(num):
+        # num = e.target
+        print('sub', num)
+
+# /////////////////////////////////////////////
+
+    prompt = input(f'Your balance is ${balance} What would you like to do? (deposit, withdraw, check balance)\n')
+
+    if prompt == 'check balance':
+        print(f'Your current balance is ${balance}')
+    elif prompt == 'withdraw':
+        question_withdraw = input('How much would you like to withdraw?')
+        print('withdraw', question_withdraw)
+        balance = balance - int(question_withdraw)
+        print('balance', balance)
+    elif prompt == 'deposit':
+        question_deposit = input('How much would you like to deposit?')
+        print('deposit', question_deposit)
+        balance = balance + int(question_deposit)
+        print('balance', balance)
+
+    question_done = input('Are  you done?')
+
+    if question_done == 'yes':
+        continue_ = 'no'
+        print('Thank you!')
+
+
+
+# Your current balance is 5000
+
+# Gather user input using the `input` function. Note that input always returns
+# user input as a string. You have to manually convert it to an int or a float
+# to make it behave like number. Also, end the input prompt with a \n newline
+# character if you want the user to type in on the next line.
+
+# ```
+# age = input("How old are you?\n")
+# age = int(age)
+# ```
+
+# Here is a sample output:
+
+# ```
+# Your current balance is
+# 4000
+# What would you like to do? (deposit, withdraw, check_balance)
+# deposit
+# How much would you like to deposit?
+# 1000
+# Your current balance is 5000
+# Are you done?
+# yes
+# Thank you!
+# ```
