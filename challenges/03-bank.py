fea6e6fcd0a5380acec22efe66c076b4139aba94
cjones1047 <cjmj1047@gmail.com>
2022-08-10 19:09:28 -0500

    03 done

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -2,25 +2,26 @@ print("Welcome to Chase bank.")
 
 balance = int(input("Your current balance is:\n"))
 
-action = input("What would you like to do? (deposit, withdraw, check_balance)\n")
+def run_app():
+    
+    action = input("What would you like to do? (deposit, withdraw, check_balance)\n")
 
-if action == "check_balance":
-    print("Your current balance is:\n{balance}")
+    if action == "withdraw":
+        withdrawal = int(input("How much would you like to withdraw?\n"))
+        print(f"Your new balance is:\n{balance - withdrawal}")
+    elif action == "deposit":
+        deposit = int(input("How much would you like to deposit?\n"))
+        print(f"Your new balance is:\n{balance + deposit}")
+    else:
+        print(f"Your current balance is:\n{balance}")
+        
     done = input("Are you done?\n")
-    if done == "no":
-        balance
+    if done == "no" or done == "No" or done == "NO":
+        run_app()
     else:
         print("Thank you!")
-# how_much = int(input("What is the second number?\n"))
 
-# if calc == 'add':
-#     print(first_number + second_number)
-# elif calc == 'sub':
-#     print(first_number - second_number)
-# elif calc == 'mult':
-#     print(first_number * second_number)
-# elif calc == 'div':
-#     print(first_number / second_number)
+run_app()
 
 print("Have a nice day!")
 
