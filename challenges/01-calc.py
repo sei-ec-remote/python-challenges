69e490402d23b669899324a9d1772489e211535d
erisha <forever.erisha@gmail.com>
2022-08-17 06:59:00 -0400

    complete

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,22 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+
+
+
+operator = input('What calculation would you like to do? (add, sub, mult, div)')
+num1 = int(input('enter 1st number'))
+num2 = int(input('enter 2nd number'))
+
+if operator == 'add':
+    print('total =', num1 + num2)
+
+elif operator == 'sub':
+    print('total =',num1 - num2)
+
+elif operator == 'mult':
+    print('total =', num1 * num2)
+
+elif operator == 'div':
+    print('total =', num1 / num2)
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,24 @@
 print("Welcome to Chase bank.")
-print("Have a nice day!")
+
+
+action = input('What would you like to do today? (deposit, withdraw, chk_balance)')
+
+balance = 4000
+
+
+if action == 'chk_balance':
+    print('Your current balance is $', balance)
+
+elif action == 'deposit':
+    deposit = input('How much would you like to deposit today? $')
+    balance = balance + int(deposit)
+    print('Your current balance is $', balance)
+
+elif action == 'withdraw':
+    withdraw = input('How much will you like to withdraw?')
+    balance = balance - int(withdraw)
+    print('Your current balnace is $', balance)
+
+else:
+    print("Have a nice day!")
 
