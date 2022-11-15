dffa45f617a9364a7d9b359ba976fe2880a99a86
sam-phillips21 <phillips.sam@gmail.com>
2022-11-14 20:20:57 -0700

    work in progress

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,35 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+print("calculator choose and option below:")
+print("1. add")
+print("2. subtract")
+print("3. multiply")
+print("4. divide") 
+
+operation = input()
+
+if operation == "1":
+    num1 = input('enter first number ')
+    num2 = input('enter second number ')
+    print( "the sum is " + str(int(num1) + int(num2)))
+    # add
+elif operation == "2":
+    num1= input('enter first number ')
+    num2= input('enter second number ')
+    print( "the difference is " + str(int(num1) - int(num2)))
+    # subtract
+elif operation == "3":
+    num1= input('enter first number ')
+    num2= input('enter second number ')
+    print( "the product is " + str(int(num1) * int(num2)))
+    #multiply
+elif operation == "4":
+    num1= input('enter first number ')
+    num2= input('enter second number ')
+    print( "the result is " + str(int(num1) / int(num2)))
+     # divide
+else:
+    print("invalid entry")
+# number1 = input(int)
+# number2 = input(int)
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,30 @@
-print("Welcome to Chase bank.")
-print("Have a nice day!")
+print("Welcome to Chase bank.\n")
+# print("Have a nice day!")
+
+print("Would you like to: \n")
+print("1. check account balance")
+print("2. withdraw cash")
+print("3. make a deposit ")
+
+operation = input()
+
+if operation == "1":
+    print("your account balance is -19,000. You broke ass\n")
+
+elif operation == "2":
+    print("insuffecient funds broke ass\n")
+
+elif operation == "3":
+    print("Gimme that money\n")
+
+print("all set ? yes : no  ")  
+
+response = input()
+
+if response == "yes":
+    print("thank you, goodbye")
+elif response == "no":
+    print("get a job broke ass")
+print()
+
 
