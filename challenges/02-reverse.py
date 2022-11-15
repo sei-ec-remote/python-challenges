6c83ac87a7eb0e6388f1e6d71a8cd1383efd1508
melanie silva <melsil11@hotmail.com>
2022-11-15 17:43:52 -0500

    completed reverese with help from online search of reverse with a for loop

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,17 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+# define the function
+def reverse(string):
+# define string
+# loop over string
+    string = [string[i] for i in range(len(string) -1, -1, -1)]
+#  return string
+    return ''.join(string)
+# define the string to be reveresd
+sample ="reverse_me"
+# print the orignal string
+print('original string is : ', sample)
+#  print the reversed string
+print('the reversed string is : ', reverse(sample))
