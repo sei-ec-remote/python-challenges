b61d715b4d75ab4ba17a2802ebe12f5f7356ebb8
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2022-11-15 17:35:04 -0600

    all files do what they should

--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,14 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+
+# Python3 program to sort letters
+# of string alphabetically
+ 
+def sortString(str):
+    return ''.join(sorted(str))
+     
+# Driver code
+str = 'PYTHON'
+print(sortString(str))
