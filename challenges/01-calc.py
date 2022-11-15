21e4acb8555923408be2439cb42f6062927f7924
melanie silva <melsil11@hotmail.com>
2022-11-15 17:28:39 -0500

    completed calc fixed answer by having an empty param

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,24 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+
+def calculator():
+    choice = input('What method they would like to use addition, subtraction, multiply, or divide?')
+
+    num1 = int(input('please input your first number.'))
+    num2 = int(input('please input your second number.'))
+    answer = ()
+
+    if choice == 'addition':
+        answer = num1 + num2
+    elif choice == 'subtraction':
+        answer = num1 - num2
+    elif choice == 'multply':
+        answer = num1 * num2
+    elif choice == 'divide':
+        answer = num1 / num2
+
+    print(f'The results are {answer}.')
+calculator()
+
