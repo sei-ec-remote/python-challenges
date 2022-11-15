71f4ed2a0ba07274fe355124c5480b89c402a507
nabbott98 <nicholasabbott98@gmail.com>
2022-11-14 21:18:04 -0500

    03-bank complete

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,31 @@
 print("Welcome to Chase bank.")
-print("Have a nice day!")
 
+balance = float(input("Your current balance is: $"))
+while True:
+    while True:
+        action = input("What would you like to do? (deposit, withdraw, check_balance): ")
+        if action.lower() not in ('deposit', 'withdraw', 'check_balance'):
+            print("Not a bank operation")
+        else:
+            break
+
+    if action == 'check_balance':
+        print('Current Balance is: $', balance)
+    else:
+        amount = input('How much would you like to ' + action + '? $')
+        if action == 'deposit':
+            balance += float(amount)
+        else:
+            balance -= float(amount)
+
+    while True:
+        answer = input('Are you done?(Yes/No): ')
+        if answer.lower() not in ('yes', 'no'):
+            print("Not an expected input")
+        else:
+            break
+
+    if answer == 'yes':
+        break
+
+print("Thank you! Have a nice day!")
