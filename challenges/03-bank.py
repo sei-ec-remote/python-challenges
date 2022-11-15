ac0e8f01e23d5d7437f9cfd298e1c31e5c2d0e26
sam-phillips21 <phillips.sam@gmail.com>
2022-11-15 14:49:20 -0700

    completed challenges

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -9,10 +9,10 @@ print("3. make a deposit ")
 operation = input()
 
 if operation == "1":
-    print("your account balance is -19,000. You broke ass\n")
+    print("your account balance is -19,000. You broke\n")
 
 elif operation == "2":
-    print("insuffecient funds broke ass\n")
+    print("insuffecient funds\n")
 
 elif operation == "3":
     print("Gimme that money\n")
@@ -24,7 +24,32 @@ response = input()
 if response == "yes":
     print("thank you, goodbye")
 elif response == "no":
-    print("get a job broke ass")
+    print("get a job, you broke")
 print()
 
 
+class BankAccount():
+    def __init__(self, acct_type, balance=0):
+        self.type = acct_type
+        self.balance = balance
+        self.overdraft_fees = 0
+
+    def __str__(self):
+        status = 'balance: {} \noverdraft fees: {}'.format(self.balance, self.overdraft_fees)
+        return status
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        if (self.balance < 0):
+            self.overdraft_fees += 20
+        return amount
+
+my_acct = BankAccount('checking', 6)
+my_acct.deposit(50)
+my_acct.withdraw(25)
+print(my_acct)
+my_acct.withdraw(35)
+print(my_acct)
--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,34 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+
+# alphabetize a string
+# Create a function that takes a string and returns the string with the letters in alphabetical order (ie. hello becomes ehllo), Assume numbers and punctuation symbols will not be included in the string.
+
+
+# words = input('youll never get out of this maze')
+# my_list = list(words)
+# my_list.sort()
+# alphabet = ''.join(my_list)
+# print(alphabet)
+
+
+# def alphabetize(string):
+#     string = list(string)
+#     string.sort()
+#     return "".join(string)
+
+# abc_string= input("youll never get out of this maze")
+
+# print(alphabetize(abc_string))
+
+    # input('youll never get out of this maze')
+    # string.split()
+
+
+def sortString(str):
+    return ''.join(sorted(str))
+     
+str = 'shablam'
+print(sortString(str))
