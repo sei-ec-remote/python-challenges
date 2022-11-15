a92ea2615d41c09e09e637969d54da4d46375d16
iceagelemonade <bryanjcarr88@gmail.com>
2022-11-14 21:57:57 -0500

    hw complete

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,21 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+def calc():
+    method = input("What calculation would you like to do? (add, sub, mult, div)\n")
+    num1 = int(input("What is number 1?\n"))
+    num2 = int(input("What is number 2?\n"))
+
+    if method == "add":
+        result = num1 + num2
+    elif method == "sub":
+        result = num1 - num2
+    elif method == "mult":
+        result = num1 * num2
+    elif method == "div":
+        result = num1 / num2
+    else:
+        return print("Illegal Opperator")
+    return print("Your result is", result)
+
+calc()
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,39 @@
 print("Welcome to Chase bank.")
-print("Have a nice day!")
+
+def deposit(balance):
+    num = int(input("How much would you like to deposit?\n"))
+    balance += num
+    check_balance(balance)
+
+def withdraw(balance):
+    num = int(input("How much would you like to withdraw?\n"))
+    if num > balance:
+        print("Insufficient Funds\n")
+        check_balance(balance)
+    else:    
+        balance -= num
+        check_balance(balance)
+
+def check_balance(balance):
+    print("Your current balance is", balance)
+    again = input("Are you done?\n")
+    if (again.lower() == "no") or (again.lower() == "n"):
+        start_transaction(balance)
+    else:
+        print("Have a nice day!")
+
+def start_transaction(balance):
+    print("Your current balance is:\n", balance)
+    action = input("What would you like to do? (deposit, withdraw, check_balance)\n")
+    if action == "deposit":
+        deposit(balance)
+    elif action == "withdraw":
+        withdraw(balance)
+    elif action == "check_balance":
+        check_balance(balance)
+    else:
+        print("Invalid Request. Bink Bonk.")
+        # start_transaction(balance)
+
+start_transaction(100)
 
