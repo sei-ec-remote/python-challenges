e43162d2f4db0572f2c350ce94e540ef72f39aa0
skbarbee <barbee.s.k@gmail.com>
2022-11-15 16:57:12 -0500

    challenge 1 done

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,23 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+
+def say_hello(name, greeting):
+	name = input('What is your name? ')
+	greeting = input ('How do you want to be greeted? ')
+	print(f"{greeting}! {name}")
+
+say_hello("name", "greeting")
+
+def area_function(x, y):
+	area = x * y
+	print(f'Your area is: {area}')
+
+
+user_x = input("What is the length of you square? ")
+user_y = input("What is the width of you square? ")
+
+area_function(int(user_x), int(user_y))
+
+
