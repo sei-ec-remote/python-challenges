6b1b3f43b9a1581c5d68a34c8ee90ef2331d4015
nabbott98 <nicholasabbott98@gmail.com>
2022-11-14 20:53:40 -0500

    01-calc complete

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,32 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+# What calculation would you like to do? (add, sub, mult, div)
+# add
+# What is number 1?
+# 3
+# What is number 2?
+# 6
+# Your result is 9
+
+while True:
+    operation = input("What calculation would you like to do? (add, sub, mult, div): ")
+    if operation.lower() not in ('add', 'sub', 'mult', 'div'):
+        print("Not an operation")
+    else:
+        break
+
+num1 = float(input("What is number 1? : "))
+num2 = float(input("What is number 2? : "))
+
+if operation == 'add':
+    solution = num1 + num2
+elif operation == 'sub':
+    solution = num1 - num2
+elif operation == 'mult':
+    solution = num1 * num2
+elif operation == 'div':
+    solution = num1 / num2
+
+print('Your result is', solution)
