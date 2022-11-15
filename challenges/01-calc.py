77a7eeb8d22dceb5aedec5edc7c9844d4449dc9c
skbarbee <barbee.s.k@gmail.com>
2022-11-15 18:09:05 -0500

    challenge 1

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -1,24 +1,35 @@
-# Use the `input()` function to prompt a user to enter something.
-# input() always returns a string value. If you ever want someone
-# to enter a number you have to use the `int()` function to convert
-# what they typed in to a string.
+# ### Challenge 1 - Calculator
+
+# Create a simple calculator that first asks the user what method they would like
+def calculator():
+	question = input("What would you like to do?\n 1. add 2. subtract. 3. multiply. 4. divide \nSelect the number of the operation you want. ")
+	num1 = input("What is number 1? ")
+	num2 = input("What is number 2? ")
+
+	if(question == "1"):
+		print(f"Your sum is:", int(num1) + int(num2))
+	elif(question == "2"):
+		print(f"Your difference is:",int(num1) - int(num2))
+	elif(question == "3"):
+		print(f"Your product is:", int(num1) * int(num2))
+	elif(question == "4"):
+		print(f"Your quotient is:", int(num1)/int(num2))
+
+calculator()
+
+# to use (addition, subtraction, multiplication, division) and then asks the user
+# for two numbers, returning the result of the method with the two numbers. Here
+# is a sample prompt:
+
+# ```
+# What calculation would you like to do? (add, sub, mult, div)
+# add
+# What is number 1?
+# 3
+# What is number 2?
+# 6
+# Your result is 9
 
 
-def say_hello(name, greeting):
-	name = input('What is your name? ')
-	greeting = input ('How do you want to be greeted? ')
-	print(f"{greeting}! {name}")
-
-say_hello("name", "greeting")
-
-def area_function(x, y):
-	area = x * y
-	print(f'Your area is: {area}')
-
-
-user_x = input("What is the length of you square? ")
-user_y = input("What is the width of you square? ")
-
-area_function(int(user_x), int(user_y))
 
 
