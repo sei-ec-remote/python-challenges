d7e63c709b30561e8a007f1925845599e0b00a8a
melanie silva <melsil11@hotmail.com>
2022-11-15 18:16:00 -0500

    completed bank, fixed the infinit while loop by adding the status to the last elif and declaring it true

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,36 @@
 print("Welcome to Chase bank.")
+
+# declare balance
+balance = 100
+# declare answer to question
+status = False
+#  declare function
+def bank():
+# globals
+    global balance
+    global status
+#  create a loop 
+    while(status != True):
+        start_question = input('Would you like to display your balance(balance), withdraw, deposit into your account, or exit?\n')
+# if statement
+# display balance
+        if(start_question == 'balance'):
+            print(f'Your balance is {balance}\n')
+#  deposit 
+        elif(start_question == 'deposit'):
+            amount = int(input('What amount would you like to deposit?\n'))
+            balance = balance + amount 
+            print(f'Your new balance is {balance}\n')
+# withdraw 
+        elif(start_question == 'withdraw'):
+            amount = int(input('What amount would you like to withdraw?\n'))
+            balance = balance - amount 
+            print(f'Your new balance is {balance}\n')
+        elif(start_question == 'exit'):
+            status = True
 print("Have a nice day!")
+
+bank()
+
+
 
