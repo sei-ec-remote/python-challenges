a4222eb89dc3d254235aa245f8b0ff95a6911786
skbarbee <barbee.s.k@gmail.com>
2022-11-15 18:17:19 -0500

    challenge 2

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -1,14 +1,20 @@
-# Python offers several ways to reverse a String. This is a classic thing
-# that lots of people want to do. It's probably easy to look up this
-# answer on Stack Overflow.
-#
-# This website of 30 Python Tips and Tricks also happens to point out
-# several ways to reverse a string, and it's a good read!
-#
-# http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+### Challenge 2 - Reverse a string
+# Reverse a string manually. Don't use s[::-1] (even though that's awesome).
+# Create a new variable storing an empty string and add the letters from
+# the first string one by one. The for loop should iterate over the length
+# of the string and you should access letters individually.
 
-def reverse_string(string):
-	return string[::-1]
-	
+# Below is some sample output.
 
-print(reverse_string("This would have been helpful in algorithms"))
+# ```
+# Enter a string:
+# reverse_me
+# em_esrever
+
+def reverse_string(str):
+	r_string = ""
+	for letter in reversed(str):
+		r_string += letter
+	print(r_string)
+
+reverse_string("This is the string in reverse")
