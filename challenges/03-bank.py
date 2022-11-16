5b2c82b7b969f80dafd440a60cfbc811665fd7f5
lozier <latayroutier@gmail.com>
2022-11-16 09:26:09 -0500

    completed challenge 03

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,27 @@
-print("Welcome to Chase bank.")
-print("Have a nice day!")
+balance = 0
 
+def bank_transactions():
+    print("Welcome to Chase bank.")
+    transaction = input("What would you like to do? (deposit, withdrawal, check_balance)")
+    global balance
+    if transaction == "deposit":
+        amount = input("How much would you like to deposit? $")
+        balance += int(amount)
+    elif transaction == "withdrawal":
+        amount = input("How much would yopu like to withdraw? $")
+        if balance < int(amount):
+            print(f"You only have ${balance}")
+        else:
+            balance -= int(amount)
+    elif transaction == "check_balance":
+        print(f"Your current balance is ${balance}")
+    else:
+        print("Please enter valid transaction type.")
+
+    complete_transaction = input("Anything else? (yes, no)")
+    if complete_transaction == "yes":
+        bank_transactions()
+    else:
+        print("Have a nice day!")
+
+bank_transactions()
