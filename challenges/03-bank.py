17a71a25b9f954e329da9243c3969ac132e81f50
saparker <stephanieparker1997@gmail.com>
2022-11-18 17:42:45 -0500

    bank complete

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,14 @@
+balance = float(20)
+
 print("Welcome to Chase bank.")
+print("Your availble balance is", balance)
+operator = input("Deposit or withdraw? ")
+if operator == 'deposit' or operator == 'Deposit':
+    amount = float(input("How big is your deposit?   "))
+    print('New Balance: ', balance + amount)
+elif operator == 'withdraw' or operator == 'Withdraw':
+    amount = float(input("How much would you like to withdraw? "))
+    print("New Balance: ", balance - amount)
+
 print("Have a nice day!")
 
