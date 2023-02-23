52234f4315a85223eb7afbb963e7f778a7d0911f
danieljo <hyundanieljo@gmail.com>
2023-02-23 18:33:21 -0500

    Challenge 4 completed

--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,11 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+
+string = 'avada kedavra'
+string = list(string)
+string.sort()
+string = ''.join(string)
+
+print(string) 
