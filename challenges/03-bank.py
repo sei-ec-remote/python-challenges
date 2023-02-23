77b0c86cd924d060b8af81d17e125cbf8b89fe96
KenGibb <kenjibbo@gmail.com>
2023-02-23 16:29:52 -0600

    added bank transactions

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,18 @@
-print("Welcome to Chase bank.")
-print("Have a nice day!")
+print("Welcome to Bank of America.")
 
+# age = input("How old are you?\n")
+# age = int(age)
+
+Balance = 23084.76
+check = input("Choose between: Deposit, Withdrawl, or Check Balance\n")
+
+if check == 'Deposit':
+    deposit = int(input("How much do you want to deposit?:\n "))
+    print("Your total balance is now: ", deposit + Balance)
+    print("Have a nice day!")
+elif check == 'Withdrawl':
+    withdraw = int(input("How much of our money are you taking?:\n "))
+    print("Your total balance is now: ", Balance - withdraw)
+    print("Have the day you deserve!")
+elif check == 'Check_Balance':
+    print("Total Balance: \n", Balance)
