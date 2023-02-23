fd9a84cafbdbfe01de237cfc695faaca00e548e3
KenGibb <kenjibbo@gmail.com>
2023-02-23 16:38:51 -0600

    sorted a string

--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,16 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+test_string = input("What should we sort? \n")
+ 
+# printing original string
+print("The original string : " + str(test_string))
+ 
+# using join() + sorted()
+# Sorting a string
+res = ''.join(sorted(test_string))
+     
+# print result
+print("String after sorting : " + str(res))
+
+# the quick brown fox jumped over the lazy gorillas
