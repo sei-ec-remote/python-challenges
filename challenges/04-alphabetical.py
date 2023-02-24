bfb3191dd234e7cbccac3d2e5b530b9a680685df
Travis <travishersh0@gmail.com>
2023-02-23 19:08:00 -0500

    Alphabetical done

--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,11 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+def order_string(string):
+    print(sorted(string))
+
+order_string('PYTHON')
+# Orders it like [' ', '!', 'H', 'W', 'd', 'e', 'l', 'l', 'l', 'o', 'o', 'r']
+# I couldn't find a way to lowercase it.
+order_string('Hello World!')
