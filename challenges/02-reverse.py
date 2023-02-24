32ad2f78fee180ba99e46573c6810d83dac339a5
bruceabrigo <bruceabrigo@outlook.com>
2023-02-23 22:45:02 -0600

    Reversed a string using the slice method with print, and in a function

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,15 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+# Reverse in line using slicing -(:: starts at then end of the string and adding -1 moves one step back)
+print('I like to go outside'[::-1])
+
+# Reverse in  a function
+def return_reverse(str):
+      pass # slice passed in string 
+      print(str[::-1])
+
+return_reverse('Reverse that!')
+
+
