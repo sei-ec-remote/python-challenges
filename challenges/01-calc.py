a1d8a07b0f6fc4edbbe237da392cf28872dcfc27
jrh <joshuaharris1103@gmail.com>
2023-02-25 14:58:42 -0500

    calc done

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,20 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+def simple_calc():
+    calc_select = input("what would you like to do? Type '+', '-', '*', or '/': ")
+    num1 = int(input('First calculated integer is: '))
+    num2 = int(input('Second calculated integer is: '))
+    if (calc_select == '+'):
+        print(num1 + num2)
+    elif (calc_select == '-'):
+        print(num1 - num2)
+    elif (calc_select == '*'):
+        print(num1 * num2)
+    elif (calc_select == '/'):
+        print(num1 / num2)
+    else:
+        return 'no valid option entered'
+
+calc_select()
