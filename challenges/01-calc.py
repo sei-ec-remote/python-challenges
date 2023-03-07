3d8f0564d92d2a8764f70b8d40152afb2e616309
MalcolmKemp <Malokingsley@icloud.com>
2023-03-07 15:48:53 -0500

    'completed'

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -3,7 +3,23 @@
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
 
-question = 'What is your name?'
-answer = input(question)
-print('Hello,', answer)
+# define the function that performs the operation
+def calculator(op, num1, num2):
+    if op == "add":
+        return num1 + num2
+    elif op == "sub":
+        return num1 - num2
+    elif op == "mult":
+        return num1 * num2
+    elif op == "div":
+        return num1 / num2
+
+# get user input for the operation and the numbers
+op = input("What calculation would you like to do? (add, sub, mult, div) ")
+num1 = float(input("What is number 1? "))
+num2 = float(input("What is number 2? "))
+
+# call the function with the user input and display the result
+result = calculator(op, num1, num2)
+print("Your result is", result)
 
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,33 @@
 print("Welcome to Chase bank.")
+# Collect the user's starting balance
+balance = 4000
+
+# Display the user's current balance
+print("Your current balance is")
+print(balance)
+
+# Ask the user what they want to do
+action = input("What would you like to do? (deposit, withdraw, check_balance)\n")
+
+# Perform the selected action
+if action == "deposit":
+    amount = float(input("How much would you like to deposit?\n"))
+    balance += amount
+elif action == "withdraw":
+    amount = float(input("How much would you like to withdraw?\n"))
+    balance -= amount
+elif action == "check_balance":
+    pass  # Do nothing
+else:
+    print("Invalid action. Please try again.")
+
+# Display the user's updated balance
+print("Your current balance is")
+print(balance)
+
+# Ask the user if they're done
+done = input("Are you done?\n")
+
+# Say goodbye
 print("Have a nice day!")
 
